e5c7ec8dc5100c0dd0dd209120136f14d02a85a7
nonononoka <onon.kaori@gmail.com>
2026-07-22 23:20:31 +0900

    part3

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -12,9 +12,27 @@ enum class StatementType {
     SELECT
 }
 
+val Pages = mutableListOf<Row>()
+
+data class Row(val id: Int, val name: String, val email: String)
+
+sealed interface Statement {
+    data class InsertStatement(override val statementType: StatementType, val rowToInsert: Row) : Statement
+    data class SelectStatement(override val statementType: StatementType) : Statement
+
+    val statementType: StatementType
+}
+
 sealed interface PrepareResult {
-    data class SUCCESS(val statementType: StatementType) : PrepareResult
+    data class INSERT_SUCCESS(val statement: Statement.InsertStatement) : PrepareResult
+    data class SELECT_SUCCESS(val statement: Statement.SelectStatement) : PrepareResult
     object Unrecognized : PrepareResult
+    object SYNTAX_ERROR : PrepareResult
+}
+
+enum class ExecuteResult {
+    EXECUTE_SUCCESS,
+    EXECUTE_TABLE_FAILURE,
 }
 
 fun doMetaCommand(command: String): MetaCommandResult {
@@ -27,25 +45,49 @@ fun doMetaCommand(command: String): MetaCommandResult {
 
 // どういうcommandかを解釈するだけ．実行はまだ
 fun prepareStatement(command: String): PrepareResult {
-    if (command == "insert") {
-        return PrepareResult.SUCCESS(StatementType.INSERT)
-    } else if (command == "select") {
-        return PrepareResult.SUCCESS(StatementType.SELECT)
+    val argList = command.split(" ")
+    if (argList[0] == "insert") {
+        if (argList.size != 4) {
+            return PrepareResult.SYNTAX_ERROR
+        }
+        val argList = command.split(" ")
+        return PrepareResult.INSERT_SUCCESS(
+            Statement.InsertStatement(
+                StatementType.INSERT,
+                Row(argList[1].toInt(), argList[2], argList[3])
+            )
+        )
+    } else if (argList[0] == "select") {
+        return PrepareResult.SELECT_SUCCESS(Statement.SelectStatement(StatementType.SELECT))
     } else {
         return PrepareResult.Unrecognized
     }
 }
 
-fun executeStatement(statementType: StatementType) {
-    when (statementType) {
-        StatementType.INSERT -> {
-            println("This is where we would do an insert.\n"); }
+fun executeInsert(row: Row): ExecuteResult {
+    Pages.add(row)
+    return ExecuteResult.EXECUTE_SUCCESS
+}
 
-        StatementType.SELECT -> {
-            println("This is where we would do an select.\n");
+fun executeSelect(): ExecuteResult {
+    for ((id, name, email) in Pages) {
+        println("id: ${id}, name: ${name}, email: ${email}")
+    }
+    return ExecuteResult.EXECUTE_SUCCESS
+}
+
+fun executeStatement(statement: Statement): ExecuteResult {
+    when (statement) {
+        is Statement.InsertStatement -> {
+            return executeInsert(statement.rowToInsert)
+        }
+
+        is Statement.SelectStatement -> {
+            return executeSelect()
         }
     }
 }
+
 
 fun main() {
     while (true) {
@@ -68,12 +110,38 @@ fun main() {
 
         // normal command
         when (val res = prepareStatement(command)) {
-            is PrepareResult.SUCCESS -> {
-                executeStatement(res.statementType)
-            }
-
             is PrepareResult.Unrecognized -> {
                 println("Unrecognized command: $command")
+                continue;
+            }
+
+            is PrepareResult.SYNTAX_ERROR -> {
+                println("Syntax.error: $command")
+                continue;
+            }
+
+            is PrepareResult.INSERT_SUCCESS -> {
+                when (executeStatement(res.statement)) {
+                    ExecuteResult.EXECUTE_SUCCESS -> {
+                        println("Executed successfully")
+                    }
+
+                    ExecuteResult.EXECUTE_TABLE_FAILURE -> {
+                        println("Table failure")
+                    }
+                }
+            }
+
+            is PrepareResult.SELECT_SUCCESS -> {
+                when (executeStatement(res.statement)) {
+                    ExecuteResult.EXECUTE_SUCCESS -> {
+                        println("Executed successfully")
+                    }
+
+                    ExecuteResult.EXECUTE_TABLE_FAILURE -> {
+                        println("Table failure")
+                    }
+                }
             }
         }
     }
